d95087f74e260d2422f3c760d9d3f5229b656d96
TobyPalmer <tobpa249@student.liu.se>
2014-12-23 15:38:37 +0100

    http request working

--- src/com/example/interlab3/MainActivity.java
+++ src/com/example/interlab3/MainActivity.java
@@ -1,9 +1,30 @@
 package com.example.interlab3;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,11 +34,36 @@ import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
+	private final String USER_AGENT = "Mozilla/5.0";
+
+	@SuppressLint("NewApi")
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)  {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
+		//Could not do make request without this if statement
+	    if (android.os.Build.VERSION.SDK_INT > 9) {
+	        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+	        StrictMode.setThreadPolicy(policy);
+	      }
+		
+		MainActivity http = new MainActivity();
+		 
+		System.out.println("Testing 1 - Send Http GET request");
+		try {
+			http.sendGet(3,"Emm");
+			System.out.println("success");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("something went wrong");
+			e.printStackTrace();
+		}
+ 
+		//System.out.println("\nTesting 2 - Send Http POST request");
+		//http.sendPost();
+		   
+		 
 	}
 
 	@Override
@@ -40,5 +86,43 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	
+	// HTTP GET request
+		private void sendGet(int id, String searchString) throws Exception {
+			
+			
+			
+			String url = "http://flask-afteach.rhcloud.com/getnames/"+ Integer.toString(id) +"/"+ searchString;
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	 
+			// optional default is GET
+			con.setRequestMethod("GET");
+	 
+			//add request header
+			con.setRequestProperty("User-Agent", USER_AGENT);
+	 
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'GET' request to URL : " + url);
+			System.out.println("Response Code : " + responseCode);
+	 
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+	 
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			
+			JSONObject json = new JSONObject(response.toString());
+	 
+			System.out.println("json length" + json.getJSONObject("result"));
+			
+			//print result
+			System.out.println(response.toString());
+	 
+		}
 
 }
